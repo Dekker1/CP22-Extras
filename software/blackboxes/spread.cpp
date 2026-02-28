fa07d703d99eb9fd6dba5ff7d1aff351a54141e0
Jip J. Dekker <jip@dekker.one>
2026-02-28 12:37:37 +1100

    fix spread value blackbox

--- software/blackboxes/spread.cpp
+++ software/blackboxes/spread.cpp
@@ -8,22 +8,23 @@ extern "C" void fzn_blackbox(const int *int_in, size_t int_in_size,
                              double *float_out, size_t float_out_size) {
   assert(int_out_size == 1);
 
-  constexpr int64_t scale = 1;
-  const int64_t n = static_cast<int64_t>(int_in_size);
+  const int64_t scale = static_cast<int64_t>(int_in[int_in_size - 1]);
+  const size_t n = int_in_size - 1;
 
   int64_t sum = 0;
-  for (size_t i = 0; i < int_in_size; ++i) {
+  for (size_t i = 0; i < n; ++i) {
     sum += static_cast<int64_t>(int_in[i]);
   }
 
   int64_t s = 0;
-  for (size_t i = 0; i < int_in_size; ++i) {
+  for (size_t i = 0; i < n; ++i) {
     const int64_t term = n * static_cast<int64_t>(int_in[i]) - sum;
     s += term * term;
   }
 
   const int64_t numerator = s * scale;
-  const int64_t denominator = n * n * n;
+  const int64_t n_ = static_cast<int64_t>(n);
+  const int64_t denominator = n_ * n_ * n_;
 
   int_out[0] = static_cast<int>(numerator / denominator);
 }
